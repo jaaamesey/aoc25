68ccfdf257a93c0bb7970b289b71dee690f689e6
James Karlsson <jameskarlsson1@gmail.com>
2025-12-01 22:34:58 +1100

    Initial day 1 solution

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,13 +1,32 @@
 pub fn part1() {
-    let input_str = include_str!("./test_input.txt");
-    let row_strs = input_str.lines();
-    let output = row_strs;
-    dbg!(output.collect::<Vec<_>>());
+    let input_str = include_str!("./real_input.txt");
+    let mut num_zeroes = 0;
+    let mut rotation = 50;
+    for l in input_str.lines() {
+        let (letter, n_str) = l.trim().split_at(1);
+        let d_rot = if letter == "L" { -1 } else { 1 } * n_str.parse::<i32>().unwrap();
+        rotation = (rotation + d_rot) % 100;
+        if rotation == 0 {
+            num_zeroes += 1;
+        }
+    }
+    dbg!(num_zeroes);
 }
 
 pub fn part2() {
-    let input_str = include_str!("./test_input.txt");
-    let row_strs = input_str.lines();
-    let output = row_strs;
-    dbg!(output.collect::<Vec<_>>());
+    let input_str = include_str!("./real_input.txt");
+    let mut num_zeroes = 0;
+    let mut rotation = 50;
+    for l in input_str.lines() {
+        let (letter, n_str) = l.trim().split_at(1);
+        let sign = if letter == "L" { -1 } else { 1 };
+        let d_rot_abs = n_str.parse::<i32>().unwrap();
+        for _ in 0..d_rot_abs {
+            rotation = (rotation + sign) % 100;
+            if rotation == 0 {
+                num_zeroes += 1;
+            }
+        }
+    }
+    dbg!(num_zeroes);
 }
