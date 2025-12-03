b2c54c8717450599beba7b2c29623dc8c719ac5e
James Karlsson <jameskarlsson1@gmail.com>
2025-12-03 23:08:13 +1100

    Part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -29,5 +29,34 @@ pub fn part1() {
 }
 
 pub fn part2() {
-    let input_str = include_str!("./test_input.txt");
+    let num_digits: usize = 12;
+    let input_str = include_str!("./real_input.txt");
+    let output = input_str
+        .lines()
+        .map(|line| {
+            let line_as_numbers = line
+                .chars()
+                .map(|c| c.to_string().parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+
+            let mut combination: usize = 0;
+            let mut i = 0;
+            for digit_idx in 0..num_digits {
+                let largest_number_index = (i..(line_as_numbers.len()
+                    - (num_digits - digit_idx - 1)))
+                    .reduce(|highest_idx, j| {
+                        if line_as_numbers[j] > line_as_numbers[highest_idx] {
+                            j
+                        } else {
+                            highest_idx
+                        }
+                    })
+                    .unwrap();
+                i = largest_number_index + 1;
+                combination = (combination * 10) + line_as_numbers[largest_number_index];
+            }
+            combination
+        })
+        .sum::<usize>();
+    dbg!(output);
 }
