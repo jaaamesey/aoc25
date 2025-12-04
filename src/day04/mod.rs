efd4806c501c1463ca180f1c5b40b58b7e915ecf
James Karlsson <jameskarlsson1@gmail.com>
2025-12-04 19:44:10 +1100

    Cleanup

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -17,8 +17,6 @@ pub fn part1() {
         (1, 1),
     ];
 
-    //  let mut dbg_valid_positions: Vec<(usize, usize)> = Vec::new();
-
     let mut outer_count = 0;
     for (y, row) in grid.iter().enumerate() {
         for (x, &is_paper) in row.iter().enumerate() {
@@ -36,31 +34,14 @@ pub fn part1() {
                 }
                 if grid[y.checked_add_signed(dy).unwrap()][x.checked_add_signed(dx).unwrap()] {
                     count += 1;
-                    // This makes it slower, why?
-                    // if count > 3 {
-                    //     break;
-                    // }
                 }
             }
             if count > 3 {
                 continue;
             }
             outer_count += 1;
-            // dbg_valid_positions.push((y, x));
         }
     }
-    // let mut dbg_str = String::new();
-    // for (y, row) in grid.iter().enumerate() {
-    //     for (x, &is_paper) in row.iter().enumerate() {
-    //         dbg_str.push(if dbg_valid_positions.contains(&(y, x)) {
-    //             'x'
-    //         } else {
-    //             if is_paper { '@' } else { '.' }
-    //         });
-    //     }
-    //     dbg!(&dbg_str);
-    //     dbg_str.clear();
-    // }
 
     dbg!(outer_count);
 }
@@ -84,19 +65,16 @@ pub fn part2() {
         (1, 1),
     ];
 
-    //  let mut dbg_valid_positions: Vec<(usize, usize)> = Vec::new();
-
     let mut outer_count = 0;
     let mut positions_to_clear: Vec<(usize, usize)> = Vec::new();
 
     loop {
-        let mut paper_found = false;
+        let prev_outer_count = outer_count;
         for (y, row) in grid.iter().enumerate() {
             for (x, &is_paper) in row.iter().enumerate() {
                 if !is_paper {
                     continue;
                 }
-                paper_found = true;
                 let mut count = 0;
                 for (dy, dx) in dirs {
                     if (y < 1 && dy < 0)
@@ -108,10 +86,6 @@ pub fn part2() {
                     }
                     if grid[y.checked_add_signed(dy).unwrap()][x.checked_add_signed(dx).unwrap()] {
                         count += 1;
-                        // This makes it slower, why?
-                        // if count > 3 {
-                        //     break;
-                        // }
                     }
                 }
                 if count > 3 {
@@ -119,30 +93,16 @@ pub fn part2() {
                 }
                 outer_count += 1;
                 positions_to_clear.push((y, x));
-                // dbg_valid_positions.push((y, x));
             }
         }
         for &(y, x) in positions_to_clear.iter() {
             grid[y][x] = false;
         }
         positions_to_clear.clear();
-        if !paper_found {
+        if prev_outer_count == outer_count {
             break;
         }
-        dbg!(outer_count);
     }
-    // let mut dbg_str = String::new();
-    // for (y, row) in grid.iter().enumerate() {
-    //     for (x, &is_paper) in row.iter().enumerate() {
-    //         dbg_str.push(if dbg_valid_positions.contains(&(y, x)) {
-    //             'x'
-    //         } else {
-    //             if is_paper { '@' } else { '.' }
-    //         });
-    //     }
-    //     dbg!(&dbg_str);
-    //     dbg_str.clear();
-    // }
 
     dbg!(outer_count);
 }
