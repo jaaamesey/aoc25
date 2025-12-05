5e705c994d1e1879e920c86f4362c2b5c42971bd
James Karlsson <jameskarlsson1@gmail.com>
2025-12-05 20:29:37 +1100

    Passing test input for part 2, but not real

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -21,4 +21,34 @@ pub fn part1() {
 
 pub fn part2() {
     let input_str = include_str!("./test_input.txt");
+    let mut ranges = Vec::<(u64, u64)>::new();
+    for line in input_str.lines() {
+        if line.is_empty() {
+            break;
+        } else {
+            let (ls, rs) = line.split_once('-').unwrap();
+            let min = ls.parse::<u64>().unwrap();
+            let max = rs.parse::<u64>().unwrap();
+
+            if let Some((existing_range_idx, (e_min, e_max))) =
+                ranges.iter().enumerate().find(|(_, (e_min, e_max))| {
+                    (min >= *e_min && min <= *e_max) || (min >= *e_min && min <= *e_max)
+                })
+            {
+                dbg!(
+                    "Merging: ",
+                    (e_min, e_max),
+                    (min, max),
+                    "to",
+                    (*e_min.min(&min), *e_max.max(&max))
+                );
+                ranges[existing_range_idx] = (*e_min.min(&min), *e_max.max(&max));
+            } else {
+                ranges.push((min, max));
+            }
+        }
+    }
+    //dbg!(&ranges);
+
+    dbg!(ranges.iter().map(|(min, max)| max - min).sum::<u64>());
 }
