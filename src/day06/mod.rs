d4968380570ca131f9a16d0068cf4251a7056bc1
James Karlsson <jameskarlsson1@gmail.com>
2025-12-06 18:24:06 +1100

    Bad part 2 (unsolved)

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -26,4 +26,57 @@ pub fn part1() {
 
 pub fn part2() {
     let input_str = include_str!("./test_input.txt");
+    let mut input_iter = input_str.lines().rev();
+    let column_ops = input_iter
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .map(|s| s.chars().next().unwrap())
+        .collect::<Vec<char>>();
+    let mut columns = column_ops
+        .iter()
+        .map(|_| Vec::new())
+        .collect::<Vec<Vec<Vec<char>>>>();
+    for line in input_iter {
+        for (column_idx, cell) in line.split_whitespace().enumerate() {
+            columns[column_idx].push(cell.chars().collect());
+        }
+    }
+    // PROBLEM: shifting matters. Need to transpose earlier.
+    dbg!(
+        columns
+            .iter_mut()
+            .enumerate()
+            .map(|(column_idx, column)| {
+                let max_digits = column.iter().fold(0, |acc, curr| acc.max(curr.len()));
+                let mut tally = if column_ops[column_idx] == '+' { 0 } else { 1 };
+                dbg!(column_ops[column_idx]);
+                // Pad columns
+                for cell in column.iter_mut() {
+                    while cell.len() < max_digits {
+                        cell.insert(0, '_');
+                    }
+                }
+                dbg!(&column);
+                for i in (0..max_digits) {
+                    let mut number = 0 as u64;
+                    for j in (0..column.len()) {
+                        if let Some(digit) = column[j].get(i) {
+                            // does this need to be reversed?
+                            if let Ok(parsed_digit) = digit.to_string().parse::<u64>() {
+                                number = (number * 10) + parsed_digit;
+                            }
+                        }
+                    }
+                    if column_ops[column_idx] == '+' {
+                        tally += number;
+                    } else {
+                        tally *= number;
+                    }
+                    dbg!(number);
+                }
+                tally
+            })
+            .sum::<u64>()
+    );
 }
