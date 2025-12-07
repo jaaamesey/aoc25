47715a143eec630b65ad4b0392f0b11f7ff63e3a
James Karlsson <jameskarlsson1@gmail.com>
2025-12-07 18:48:42 +1100

    Part 2 done

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -1,4 +1,4 @@
-use hashbrown::HashSet;
+use hashbrown::{HashMap, HashSet};
 
 pub fn part1() {
     let input_str = include_str!("./real_input.txt");
@@ -33,5 +33,40 @@ pub fn part1() {
 }
 
 pub fn part2() {
-    let input_str = include_str!("./test_input.txt");
+    let input_str = include_str!("./real_input.txt");
+    let mut input_iter = input_str.lines().enumerate();
+    let start_x = input_iter
+        .next()
+        .unwrap()
+        .1
+        .chars()
+        .enumerate()
+        .find(|(_, char)| *char == 'S')
+        .unwrap()
+        .0;
+
+    let mut beam_x_positions = HashMap::with_capacity(20);
+    beam_x_positions.insert(start_x, 1);
+
+    for (_, line_original) in input_iter {
+        // TODO: Avoid this
+        let line = line_original.chars().collect::<Vec<_>>();
+        for (x, char) in line.iter().enumerate() {
+            if *char != '^' {
+                continue;
+            }
+            let num_beams = *beam_x_positions.get(&x).unwrap_or(&0);
+            if num_beams < 1 {
+                continue;
+            }
+
+            beam_x_positions.remove(&x);
+            let existing_left = *beam_x_positions.get(&(x - 1)).unwrap_or(&0);
+            let existing_rght = *beam_x_positions.get(&(x + 1)).unwrap_or(&0);
+            beam_x_positions.insert(x - 1, existing_left + num_beams);
+            beam_x_positions.insert(x + 1, existing_rght + num_beams);
+        }
+    }
+    dbg!(&beam_x_positions);
+    dbg!(&beam_x_positions.values().sum::<usize>());
 }
